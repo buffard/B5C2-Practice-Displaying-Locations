d9b465ef328584b9b97b2dbbfb098054ee1af333
buffard <samuelwebber19@gmail.com>
2018-11-16 14:42:56 -0600

    boiler plate

---
+++ src/components/LocationList/LocationList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class LocationList extends Component {
+  render() {
+      return (
+          <div>
+              <h2></h2>
+              <h4>Nashville North Location</h4>
+              <h5>500 Puppy Way</h5>
+
+              <h4>Sam's House</h4>
+              <h5>903 S 14th St</h5>
+          </div>
+      )
+  }
+}
