2143cc4e0bd17b36ff1b6ac8baa14f22f1fd3db0
buffard <samuelwebber19@gmail.com>
2018-11-16 15:36:16 -0600

    working & completed pratice

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 
-
-export default class EmployeeList  extends Component {
-    render() {
-        return (
-            <article>
-                <h1>Employee List</h1>
-                <section>Jessica Younker</section>
-                <section>Jordan Nelson</section>
-                <section>Zoe LeBlanc</section>
-                <section>Blaise Roberts</section>
-            </article>
-        )
-    }
+class EmployeeList extends Component {
+  render() {
+    return (
+      <section className="employees">
+      <h2>Employees</h2>
+      {this.props.employees.map(employee =>
+        <div key={employee.id}>
+          <li>{employee.name}</li>
+        </div>
+      )}
+    </section>
+    )
+  }
 }
+
+export default EmployeeList
